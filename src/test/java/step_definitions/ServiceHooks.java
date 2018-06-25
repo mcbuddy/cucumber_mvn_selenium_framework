a3234fdbe31af7c32ccc0c642621ba2c670ac932
Budi Sugianto <budi.sugianto@flocabulary.com>
2018-06-25 12:45:39 -0400

    Add Selenium Webdriver to test framework

--- src/test/java/step_definitions/ServiceHooks.java
+++ src/test/java/step_definitions/ServiceHooks.java
@@ -3,20 +3,31 @@ package step_definitions;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class ServiceHooks {
     @Before
     public void initializeTest(){
-        // Code to setup initial configurations
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
     }
 
     @After
     public void embedScreenshot(Scenario scenario) {
-        if (scenario.isFailed()) {
+        // Generate the error log if the test failed
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
             try {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            System.out.println("Test Failed");
         }
+
+        // Close the browser
+        driver.close();
     }
 }
